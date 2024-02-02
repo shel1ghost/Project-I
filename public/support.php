3486db11fae400ce8c42b53fb404235f431136c0
Babi Thakur <astra@localhost.localdomain>
2024-02-02 13:34:18 +0545

    Updated code

---
+++ public/support.php
@@ -0,0 +1,71 @@
+<DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Support | CipherShield</title>
+    <link rel="stylesheet" href="./css/support.css"/>
+</head>
+<body>
+    <header>
+        <div class="logo"></div>
+        <div class="support_title">
+            <h1>Help &amp; Support - CipherShield</h1>
+        </div>
+        <div class="logout"><a href="logout.php">Logout</a></div>
+    </header>
+    <div class="main">
+        <div class="faqs">
+            <h2>CipherShield - FAQs</h2>
+            <p>
+                <b>Is CipherShield safe to use?</b>
+                <br/>
+                <span>CipherShield uses the best modern algorithm available
+                    today to protect users passwords. It is very much concerned
+                    with security regarding it's users as well as the application.
+                </span>
+            </p>
+            <p>
+                <b>How do I change CipherShield's password?</b>
+                <br/>
+                <span>CipherShield uses the best modern algorithm available
+                    today to protect users passwords. It is very much concerned
+                    with security regarding it's users as well as the application.
+                </span>
+            </p>
+            <p>
+                <b>How do I change CipherShield's password?</b>
+                <br/>
+                <span>CipherShield uses the best modern algorithm available
+                    today to protect users passwords. It is very much concerned
+                    with security regarding it's users as well as the application.
+                </span>
+            </p>
+            <p>
+                <b>How do I change CipherShield's password?</b>
+                <br/>
+                <span>CipherShield uses the best modern algorithm available
+                    today to protect users passwords. It is very much concerned
+                    with security regarding it's users as well as the application.
+                </span>
+            </p>
+        </div>
+        <div class="contact">
+            <h2>Password Security Tips from CipherShield</h2>
+            <ul>
+                <li>Always create a strong password for any kind of application.</li>
+                <li>Never use the same password for every website or applications.</li>
+                <li>Never share your passwords.</li>
+                <li>Never open any suspicious links.</li>
+                <li>Keep changing your passwords from time to time.</li>
+                <li>Always use multi-factor authentication.</li>
+            </ul>
+            <h2>Contact Information for CipherShield</h2>
+            <ul>
+                <li><b>Support Contact Number:</b> +977 9802356806</li>
+                <li><b>Email:</b> support@ciphershield.com</li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
--- public/view_passwords.php
+++ public/view_passwords.php
@@ -14,5 +14,77 @@
         </div>
         <div class="logout"><a href="logout.php">Logout</a></div>
     </header>
+    <div class="main">
+        <table>
+            <thead>
+                <tr>
+                    <th class="application_name">Application Name</th>
+                    <th class="user_id">UserID</th>
+                    <th class="password_heading">Password</th>
+                    <th class="category">Category</th>
+                    <th class="sec_qna">Security QN/A</th>
+                    <th class="twofa">2FA Info</th>
+                    <th class="actions_heading">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class="name_row">Facebook</td>
+                    <td>miss.programmerr</td>
+                    <td>apnabanale123@#$</td>
+                    <td>Social</td>
+                    <td>None</td>
+                    <td>Samsung device approval or Pin code number</td>
+                    <td><button class="edit_btn">Edit</button><button class="delete_btn">Delete</button></td>
+                </tr>
+                <tr>
+                    <td class="name_row">Facebook</td>
+                    <td>miss.programmerr</td>
+                    <td>apnabanale123@#$</td>
+                    <td>Social</td>
+                    <td>None</td>
+                    <td>Samsung device approval or Pin code number</td>
+                    <td><button class="edit_btn">Edit</button><button class="delete_btn">Delete</button></td>
+                </tr>
+                <tr>
+                    <td class="name_row">Facebook</td>
+                    <td>miss.programmerr</td>
+                    <td>apnabanale123@#$</td>
+                    <td>Social</td>
+                    <td>None</td>
+                    <td>Samsung device approval or Pin code number</td>
+                    <td><button class="edit_btn">Edit</button><button class="delete_btn">Delete</button></td>
+                </tr>
+                <tr>
+                    <td class="name_row">Facebook</td>
+                    <td>miss.programmerr</td>
+                    <td>apnabanale123@#$</td>
+                    <td>Social</td>
+                    <td>None</td>
+                    <td>Samsung device approval or Pin code number</td>
+                    <td><button class="edit_btn">Edit</button><button class="delete_btn">Delete</button></td>
+                </tr>
+                <tr>
+                    <td class="name_row">Facebook</td>
+                    <td>miss.programmerr</td>
+                    <td>apnabanale123@#$</td>
+                    <td>Social</td>
+                    <td>None</td>
+                    <td>Samsung device approval or Pin code number</td>
+                    <td><button class="edit_btn">Edit</button><button class="delete_btn">Delete</button></td>
+                </tr>
+                <tr>
+                    <td class="name_row">Facebook</td>
+                    <td>miss.programmerr</td>
+                    <td>apnabanale123@#$</td>
+                    <td>Social</td>
+                    <td>None</td>
+                    <td>Samsung device approval or Pin code number</td>
+                    <td><button class="edit_btn">Edit</button><button class="delete_btn">Delete</button></td>
+                </tr>
+                
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>
